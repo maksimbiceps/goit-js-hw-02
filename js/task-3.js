b60d1c2eb3753f73723671fbb89e7cf0823aa5a6
Maksim <Maksimka.Kravecz@gmail.com>
2026-09-15 10:03:22 +0300

    homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,18 @@
 'use strict';
 
 
-function getElementWidth(content, padding, border) {
+function checkForSpam(message) {
+
+    const lowerMessage = message.toLowerCase(); 
+    return lowerMessage.includes('spam') || lowerMessage.includes('sale');
+
+ }
     
-    return content + padding * 2 + border * 2;
 
-    
-}
-
-
-console.log(getElementWidth(50, 8, 4)); // 74
-console.log(getElementWidth(60, 12, 8.5)); // 101
-console.log(getElementWidth(200, 0, 0)); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
